e75fb3098510f534a6f28d65d75a5c9156e08a8e
affinityfinanceDAO <96967220+affinityfinanceDAO@users.noreply.github.com>
2023-06-06 02:43:28 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,33 +15,9 @@ import os
 import time
 import random
 import gc
+from class_app import BAparallel
 
 
-def process_seed_phrases(seed_phrases):
-    with multiprocessing.Pool() as pool:
-        results = pool.imap_unordered(process_seed_phrase, seed_phrases)
-        return list(results)
-
-# Define the function to generate a private key from a seed phrase and derivation path
-def generate_private_key(seed_phrase, derivation_path):
-    seed = hashlib.pbkdf2_hmac('sha512', seed_phrase.encode('utf-8'), b'Bitcoin seed', 2048)
-    master_key = hmac.new(b"Bitcoin seed", seed, hashlib.sha512).digest()
-    child_key = hmac.new(master_key[32:], b'\x00' + master_key[:32] + derivation_path.encode('utf-8'), hashlib.sha512).digest()
-    return child_key[:32]
-
-def process_seed_phrase(seed_phrase):
-    private_key = generate_private_key(seed_phrase, "m/44'/0'/0'")
-    sk = ecdsa.SigningKey.from_string(private_key, curve=ecdsa.SECP256k1)
-    vk = sk.get_verifying_key()
-    public_key = b"\x04" + vk.to_string()
-    ripemd160 = hashlib.new("ripemd160")
-    ripemd160.update(hashlib.sha256(public_key).digest())
-    hash160 = ripemd160.digest()
-    version = b"\x00"
-    payload = version + hash160
-    checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
-    address = base58.b58encode(payload + checksum).decode("utf-8").lower()
-    return seed_phrase, address
 
 def file_download(results):
     # Extract the seed phrases and addresses from the results
@@ -80,8 +56,8 @@ def main():
                     # Submit tasks for Bitcoin address generation
                     for j in range(0, num_seed_phrases, chunk_size):
                         chunk = seed_phrases[j: j + chunk_size]
-                        #results = process_seed_phrases(chunk)
-                        results = process_seed_phrases(chunk)
+                        #results = BAparallel.process_seed_phrases(chunk)
+                        results = BAparallel.process_seed_phrases(chunk)
 
                         # Process and save any remaining results
                         if results:
