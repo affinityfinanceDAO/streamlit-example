05e39641e441eefd0ab00ee41b400b629ebc8ecf
affinityfinanceDAO <96967220+affinityfinanceDAO@users.noreply.github.com>
2023-06-09 12:17:19 +0530

    class_app.py

--- class_app.py
+++ class_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from concurrent.futures import ThreadPoolExecutor,ProcessPoolExecutor,as_completed
 from mnemonic import Mnemonic
 import hashlib
 import hmac
@@ -8,39 +7,30 @@ import base58
 import base64
 import ecdsa
 import multiprocessing
-from pathos.multiprocessing import ProcessPool
-import bitcoin as btc
 import concurrent.futures
 import os
 import time
 import random
 import gc
-
+from bip32utils import BIP32Key ,BIP32_HARDEN
 
 class BAparallel:
-    @staticmethod
-    def generate_private_key(seed_phrase, derivation_path):
-        seed = hashlib.pbkdf2_hmac('sha512', seed_phrase.encode('utf-8'), b'Bitcoin seed', 2048)
-        master_key = hmac.new(b"Bitcoin seed", seed, hashlib.sha512).digest()
-        child_key = hmac.new(master_key[32:], b'\x00' + master_key[:32] + derivation_path.encode('utf-8'),
-                             hashlib.sha512).digest()
-        return child_key[:32]
-
+        @staticmethod
+    def process_root_key(seed_phrase):
+        seed_bytes = memo.to_seed(seed_phrase)
+        root_key_obj = BIP32Key.fromEntropy(seed_bytes)
+        root_key = root_key_obj.ChildKey(44 + BIP32_HARDEN).ChildKey(0 + BIP32_HARDEN).ChildKey(0 + BIP32_HARDEN).ChildKey(0)
+        return root_key
+    
     @staticmethod
     def process_seed_phrase(seed_phrase):
-        private_key = BAparallel.generate_private_key(seed_phrase, "m/44'/0'/0'")
-        sk = ecdsa.SigningKey.from_string(private_key, curve=ecdsa.SECP256k1)
-        vk = sk.get_verifying_key()
-        public_key = b"\x04" + vk.to_string()
-        ripemd160 = hashlib.new("ripemd160")
-        ripemd160.update(hashlib.sha256(public_key).digest())
-        hash160 = ripemd160.digest()
-        version = b"\x00"
-        payload = version + hash160
-        checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
-        address = base58.b58encode(payload + checksum).decode("utf-8").lower()
-        #address = "1dw9ztejmrmj1emh1dieldjpkeck2jebm6"
-        return seed_phrase, address
+        root_key = BAparallel2.process_root_key(seed_phrase)
+        results = []
+        for index in range(4):
+            child_key = root_key.ChildKey(index)
+            address = child_key.Address().lower()
+            results.append((seed_phrase, address))
+        return results
 
     @staticmethod
     def process_seed_phrases(seed_phrases):
