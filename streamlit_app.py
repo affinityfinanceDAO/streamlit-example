7e400401f26f0e2d6b4cee326ec2c26df6994fa6
affinityfinanceDAO <96967220+affinityfinanceDAO@users.noreply.github.com>
2023-07-14 20:02:30 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,69 +1,94 @@
 import streamlit as st
-import pandas as pd
-from concurrent.futures import ThreadPoolExecutor,ProcessPoolExecutor,as_completed
-from mnemonic import Mnemonic
-import hashlib
-import hmac
-import base58
-import base64
-import ecdsa
-import multiprocessing
-from pathos.multiprocessing import ProcessPool
-import bitcoin as btc
-import concurrent.futures
+import sys
+import hashlib, base58
+from coincurve.keys import PublicKey
 import os
-import time
-import random
-import gc
-from class_app import BAparallel
 
 
 
-def file_download(results):
-    # Extract the seed phrases and addresses from the results
-    seed_phrases, addresses = zip(*results)
-    # Create a DataFrame from the seed phrases and addresses
-    df = pd.DataFrame({"seed_phrase": seed_phrases, "address": addresses})
-    csv = df.to_csv(index = False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="seed_address.csv">Download CSV file</a>'
-    return href
+def getAddress(pub_key_hashed_bytes):
+    fullkey = b"\x00" + pub_key_hashed_bytes
+    sha256a = hashlib.sha256(fullkey).digest()
+    sha256b = hashlib.sha256(sha256a).digest()
+    checksum = sha256b[:4]
+    return base58.b58encode(fullkey+checksum).decode()
 
+
+
+def getPubKeyFaster(priv_key_bytes):
+    return PublicKey.from_valid_secret(priv_key_bytes)
+
+
+def getPubKeyFullUncompressedFaster(pub_key_bytes):
+    return pub_key_bytes.format(compressed=False)
+
+
+def getPubKeyFullCompressedFaster(pub_key_bytes):
+    return pub_key_bytes.format(compressed=True)
+
+# Perform SHA-256 and RipeMD160 hash functions
+def getPubKeyHashed(pub_key_full_bytes):
+    sha256 = hashlib.sha256(pub_key_full_bytes).digest()
+    ripemd160 = hashlib.new('ripemd160')
+    ripemd160.update(sha256)
+    return ripemd160.digest()
+
+
+def getRawAddress(bitcoinAddress):
+    try:
+        hex = base58.b58decode(bitcoinAddress)
+        return hex[1:len(hex) - 4]
+    except ValueError:
+        return None
+
+
+def createHashedPubKeySetFromAddressList(filename):
+    addressList = []
+    with open(filename) as f:
+        for line in f:
+            address = line.strip('\n')
+            rawAddress = getRawAddress(address)
+            if rawAddress is not None:
+                addressList.append(rawAddress)
+    return set(addressList)
+
+
+def searchInList(priv_key, hash160, rawAddressSet, compression):
+    if hash160 in rawAddressSet:
+        address = getAddress(hash160)
+        message = 'YEAH! Address %s is %s and has a private key: %s' % (address, compression, priv_key.hex())
+        st.write(message)
+
+
+
+def seek(hash160Set):
+    LOG_EVERY_N = 100000
+    i = 0
+    while True:
+        i += 1
+        priv_key = os.urandom(32)
+        publ_key = getPubKeyFaster(priv_key)
+        publ_key_uncompressed = getPubKeyFullUncompressedFaster(publ_key)
+        hash160 = getPubKeyHashed(publ_key_uncompressed)
+        searchInList(priv_key, hash160, hash160Set, 'uncompressed')
+        publ_key_compressed = getPubKeyFullCompressedFaster(publ_key)
+        hash160 = getPubKeyHashed(publ_key_compressed)
+        searchInList(priv_key, hash160, hash160Set, 'compressed')
+        if (i % LOG_EVERY_N) == 0:
+            st.write('Total Scan:',  str(i))
+            sys.stdout.flush()
+            continue
 
 
 def main():
-    st.title("BA Generator")
-    st.sidebar.title("Settings")
-    number_files = st.sidebar.number_input("Number of Files", value=50, min_value=1, step=1)
-    num_seed_phrases = st.sidebar.number_input("Number of SPs", value=25000, min_value=1, step=100000)
-    chunk_size = st.sidebar.number_input("Number of Chunk size", value=25000, min_value=1, step=1)
-    if st.button("Generate BA"):
-        progress_bar = st.progress(0)
-        with st.spinner("Generating BAs..."):
-            mnemonic = Mnemonic("english")
-            for i in range(number_files):
-                start_time = time.time()
-                with ProcessPoolExecutor() as executor:
-                    futures = []
-                    for j in range(0, num_seed_phrases, chunk_size):
-                        chunk = [mnemonic.generate() for _ in range(chunk_size)]
-                        future = BAparallel.process_seed_phrases(chunk)
-                        results1 = future
-                        results = []
-                        for result in results1:
-                            results.extend(result)
-                        if results:
-                           st.markdown(file_download(results), unsafe_allow_html=True)
-                end_time = time.time()
-                elapsed_time = end_time - start_time
-                st.write(f"File {i + 1}/{number_files} saved. Elapsed time: {elapsed_time:.2f} seconds")
-                progress_bar.progress((i + 1) / number_files)
-                del elapsed_time, start_time, end_time, chunk,results #,completed_futures,futures,
-                gc.collect()
-
-        st.success("BA generation complete!")
+    st.title("BA Search")
+    addressFile = 'D://shiva//btc_folder2//addresses11.txt'
+    if addressFile and st.button("Start Search"):
+        hash160Set = createHashedPubKeySetFromAddressList(addressFile)
+        st.write(f"File {addressFile} loaded, number of unique addresses: {len(hash160Set)}")
+        seek(hash160Set)
 
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-    gc.collect()
